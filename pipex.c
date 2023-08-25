b266a145a5f7bf464f5a39bb0e1f7d0166a66b6e
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car2s5.42barcelona.com>
2023-08-25 04:09:50 +0200

    idea for the project raised only i have to implement it

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 19:36:35 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/08/24 14:03:48 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/08/25 04:09:04 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,44 @@ char	*find_path(char **envp)
 	return (*envp + 5);
 }
 
+void	find_cmd()
+{
+	//añadir a cada env una / y el comando para buscarlo con access, importante liberar y si no lo encuentra liberar y salir
+}
+
+void	first_child(t_pipe pipex, char **argv)
+{
+	dup2(pipex.infile, 0);
+	dup2(pipex.tube[1], 1);
+	close(pipex.tube[0]);
+	pipex.cmd_args = ft_split(argv[2], ' ');
+	//si lo encuentra llamar a execve
+}
+
+void	close_files(int infile, int outfile)
+{
+	close(infile);
+	close(outfile);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	int		infile;
-	int		outfile;
-	int		permission;
-	char	**routes;
+	t_pipe	pipex;
 
 	if (argc == 5)
 	{
-		infile = open(argv[1], O_RDONLY);
-		permission = access(argv[1], F_OK | R_OK);
-		if (infile == -1 || permission == -1)
+		pipex.infile = open(argv[1], O_RDONLY);
+		pipex.permission = access(argv[1], F_OK | R_OK);
+		if (pipex.infile == -1 || pipex.permission == -1)
 			return (-1);
-		outfile = open(argv[argc - 1], O_TRUNC | O_CREAT, 0666);
-		permission = access(argv[1], W_OK);
-		if (outfile == -1 || permission == -1)
+		pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT, 0666);
+		pipex.permission = access(argv[1], W_OK);
+		if (pipex.outfile == -1 || pipex.permission == -1)
 			return (-1);
-		routes = ft_split(find_path(envp), ':');
-		while (*routes++)
-			printf("%s\n", *routes);
-		close(infile);
-		close(outfile);
+		if (pipe(pipex.tube) == -1)
+			return (-1);
+		pipex.routes = ft_split(find_path(envp), ':');
+		//hacer hijo con fork y llamar a función hijo en los dos casos con sus respectivos dup2
+		close_files(pipex.infile, pipex.outfile);
 	}
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 13:37:28 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/08/24 13:52:45 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/08/25 04:02:26 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+typedef struct s_pipe
+{
+	int		infile;
+	int		outfile;
+	int		permission;
+	char	**routes;
+	pid_t	proc1;
+	pid_t	proc2;
+	int		tube[2];
+	char	**cmd_args;
+	char	*cmd;
+}	t_pipe;
