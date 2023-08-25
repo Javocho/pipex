4d7dd74069648ab5749b92a45da8f35a7f745ec3
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car11s2.42barcelona.com>
2023-08-25 21:50:43 +0200

    first parent done and execve

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 19:36:35 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/08/25 04:09:04 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/08/25 21:15:51 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,36 @@ char	*find_path(char **envp)
 	return (*envp + 5);
 }
 
-void	find_cmd()
+char *	find_cmd(char **routes, char *cmd)
 {
-	//añadir a cada env una / y el comando para buscarlo con access, importante liberar y si no lo encuentra liberar y salir
+	char	*tmp;
+	char	*cmdroute;
+
+	while(*routes)
+	{
+		tmp = ft_strjoin(*routes, '/');
+		cmdroute = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (access(tmp, F_OK | X_OK) == 0)
+			return (cmdroute);
+		free(cmdroute);
+		return (NULL);
+	}
 }
 
-void	first_child(t_pipe pipex, char **argv)
+void	first_child(t_pipe pipex, char **argv, char **envp)
 {
+	char	*cmd;
+
 	dup2(pipex.infile, 0);
 	dup2(pipex.tube[1], 1);
 	close(pipex.tube[0]);
 	pipex.cmd_args = ft_split(argv[2], ' ');
-	//si lo encuentra llamar a execve
+	pipex.cmd = find_cmd(pipex.routes, argv[1]);
+	if (!pipex.cmd)
+		return ; //tendría que liberar cositas
+	else
+		execve(pipex.cmd, pipex.cmd_args, envp);
 }
 
 void	close_files(int infile, int outfile)
@@ -56,7 +74,14 @@ int	main(int argc, char **argv, char **envp)
 		if (pipe(pipex.tube) == -1)
 			return (-1);
 		pipex.routes = ft_split(find_path(envp), ':');
+		pipex.proc1 = fork();
+		if (pipex.proc1 == 0)
+			first_child(pipex, argv, envp);
+		pipex.proc2 = fork();
+		//if (pipex.proc2 == 0)
+			//second_child();
 		//hacer hijo con fork y llamar a función hijo en los dos casos con sus respectivos dup2
+		//mirar mejor fork y pipe
 		close_files(pipex.infile, pipex.outfile);
 	}
 }
